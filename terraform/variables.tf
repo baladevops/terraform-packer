d3a2814004f6b727ec29e0a41a3c71c9bb2b3ca6
baladevops <bala4rtraining@gmail.com>
2018-10-19 17:27:21 +0000

    variables 2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 ###### AZURE section start
 
-variable "client_id" { "ca9d7487-c484-49a2-b7a0-4b0a4cf12d9b" }
-variable "client_secret" { "bcc0f396-5a5e-4231-a951-c7e8677f7933" }
-variable "tenant_id" { "2c1f8453-5cc2-44f7-a93f-49ab55dcaab8" }
-variable "subscription_id" { "3216b735-086e-46b2-9363-44095f8213e1" }
+variable "client_id" {  default = "ca9d7487-c484-49a2-b7a0-4b0a4cf12d9b" }
+variable "client_secret" {  default = "bcc0f396-5a5e-4231-a951-c7e8677f7933" }
+variable "tenant_id" { default = "2c1f8453-5cc2-44f7-a93f-49ab55dcaab8" }
+variable "subscription_id" { default = "3216b735-086e-46b2-9363-44095f8213e1" }
 
 variable "computer_name" {
   description = "Machine_name"
